d4bc8af7a3f618f376b8b853ef4a641ecbf29a25
Mingjie Jiang <>
2021-04-05 16:05:19 +0000

    Finish email logic

--- bot.py
+++ bot.py
@@ -11,6 +11,11 @@ from datetime import datetime
 import utils as utils
 import re
 import asyncio
+from replit import db
+import random
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
+from python_http_client.exceptions import HTTPError
 
 # load environment variables
 from dotenv import load_dotenv
@@ -21,6 +26,7 @@ GUILD_ID = os.getenv('DISCORD_GUILD_ID')
 
 # configuration variables
 PENDING_ROLE_ID = int(os.getenv('PENDING_ROLE_ID'))
+MEMBER_ROLE_ID = int(os.getenv('MEMBER_ROLE_ID'))
 LOG_CHANNEL_ID = int(os.getenv('LOG_CHANNEL_ID'))
 VERIFICATION_CATEGORY_ID = int(os.getenv('VERIFICATION_CATEGORY_ID'))
 VALID_DOMAIN = os.getenv("VALID_DOMAIN")
@@ -41,12 +47,16 @@ class Status(Enum):
     unknown = discord.Color.light_gray()
 
 
-async def spit_log(msg, title=f"New Log - {datetime.now().astimezone().strftime('%d/%m/%Y %H:%M:%S %Z')}", status=Status.unknown):
+async def spit_log(
+        msg,
+        title=f"New Log - {datetime.now().astimezone().strftime('%d/%m/%Y %H:%M:%S %Z')}",
+        status=Status.unknown):
     log_channel = bot.get_channel(LOG_CHANNEL_ID)
     # spits a line of log in the logging channel
     if msg:
-        embedded_message = discord.Embed(
-            title=title, description=msg, color=status.value)
+        embedded_message = discord.Embed(title=title,
+                                         description=msg,
+                                         color=status.value)
         log_message = await log_channel.send(embed=embedded_message)
         return log_message
 
@@ -66,30 +76,41 @@ async def init_onboard(member):
     # 1. assign pending role
     pending_role = discord.utils.get(guild.roles, id=PENDING_ROLE_ID)
     await member.add_roles(pending_role)
-    await spit_log(f'{member.mention} pending role assigned. Executing onboarding flow.',
-                   title=f"⏬ Starting to onboard {member.name}... Step 1 complete!", status=Status.success)
+    await spit_log(
+        f'{member.mention} pending role assigned. Executing onboarding flow.',
+        title=f"⏬ Starting to onboard {member.name}... Step 1 complete!",
+        status=Status.success)
 
     # 2. create and assign temp role & channel
     onboard_role = await guild.create_role(name="o-" + code)
     await member.add_roles(onboard_role)
     channel_config_overwrites = {
-        guild.default_role: discord.PermissionOverwrite(view_channel=False),
-        pending_role: discord.PermissionOverwrite(view_channel=False),
-        onboard_role: discord.PermissionOverwrite(
-            view_channel=True, read_messages=True, send_messages=True)
+        guild.default_role:
+        discord.PermissionOverwrite(view_channel=False),
+        pending_role:
+        discord.PermissionOverwrite(view_channel=False),
+        onboard_role:
+        discord.PermissionOverwrite(view_channel=True,
+                                    read_messages=True,
+                                    send_messages=True)
     }
-    verification_category = discord.utils.get(
-        guild.categories, id=VERIFICATION_CATEGORY_ID)
-    onboard_channel = await guild.create_text_channel(name=code, category=verification_category, overwrites=channel_config_overwrites)
+    verification_category = discord.utils.get(guild.categories,
+                                              id=VERIFICATION_CATEGORY_ID)
+    onboard_channel = await guild.create_text_channel(
+        name=code,
+        category=verification_category,
+        overwrites=channel_config_overwrites)
     await spit_log(f'Role {code} created and assigned for {member.mention}.',
-                   title=f"⏬ Onboarding {member.name}... Step 2 complete!", status=Status.success)
+                   title=f"⏬ Onboarding {member.name}... Step 2 complete!",
+                   status=Status.success)
 
     # 3. send welcome messages
-    welcome_message = await onboard_channel.send(f'Welcome to **Build the Future Community**, {member.mention}! ' +
-                                                 'This is your personal onboarding channel. I\'m Bouncer, the gatekeeper for BTF, and I will be guiding you through the process.' + 
-                                                 '\n\nIf at any time, you feel like something is not working, ' +
-                                                 'please come back to this message and click the 🆘 button, and a staff member will be on their way to help you! ' +
-                                                 '\n\n(Click the :wave: below to begin!)')
+    welcome_message = await onboard_channel.send(
+        f'Welcome to **Build the Future Community**, {member.mention}! ' +
+        'This is your personal onboarding channel. I\'m Bouncer, the gatekeeper for BTF, and I will be guiding you through the process.'
+        + '\n\nIf at any time, you feel like something is not working, ' +
+        'please come back to this message and click the 🆘 button, and a staff member will be on their way to help you! '
+        + '\n\n(Click the :wave: below to begin!)')
     await welcome_message.add_reaction('👋')
     await welcome_message.add_reaction('🆘')
 
@@ -98,8 +119,10 @@ async def init_onboard(member):
     def check_welcome(reaction, user):
         return user == member and str(reaction.emoji) == '👋'
 
-    welcome_pending_log = await spit_log(f'{member.mention} ready to start onboarding. Waiting for reaction...',
-                                         title=f"⏸ Onboarding {member.name}... Step 3 pending...", status=Status.warning)
+    welcome_pending_log = await spit_log(
+        f'{member.mention} ready to start onboarding. Waiting for reaction...',
+        title=f"⏸ Onboarding {member.name}... Step 3 pending...",
+        status=Status.warning)
 
     await bot.wait_for('reaction_add', check=check_welcome)
     await onboard_channel.send('Waving back at ya! 👋 One second...')
@@ -107,45 +130,61 @@ async def init_onboard(member):
     await welcome_pending_log.delete()
 
     await spit_log(f'{member.mention} onboarding started!',
-                   title=f"⏬ Onboarding {member.name}... Step 3 complete!", status=Status.success)
+                   title=f"⏬ Onboarding {member.name}... Step 3 complete!",
+                   status=Status.success)
 
     await pause(onboard_channel, 2)
-    await onboard_channel.send('In order to allow you to enter our Discord community, we need to complete a few quick onboarding steps...')
+    await onboard_channel.send(
+        'In order to allow you to enter our Discord community, we need to complete a few quick onboarding steps...'
+    )
     await pause(onboard_channel, 1)
-    await onboard_channel.send('First, please read our **Code of Conduct**. Breaking any rules in the Code of Conduct will result in your prompt removal from the BTF Community.')
+    await onboard_channel.send(
+        'First, please read our **Code of Conduct**. Breaking any rules in the Code of Conduct will result in your prompt removal from the BTF Community.'
+    )
     await pause(onboard_channel, 1)
-    await onboard_channel.send('https://docs.google.com/document/d/1H9ammGSeypqZufuMT3Qn8Lcb-gBPxxoZ3PjDrDcUZis/edit')
+    await onboard_channel.send(
+        'https://docs.google.com/document/d/1H9ammGSeypqZufuMT3Qn8Lcb-gBPxxoZ3PjDrDcUZis/edit'
+    )
     await pause(onboard_channel, 2)
-    await onboard_channel.send('**Please respond with `agree` to acknowledge the Code of Conduct.**')
+    await onboard_channel.send(
+        '**Please respond with `agree` to acknowledge the Code of Conduct.**')
 
     # 4. wait for acknowledgement on CoC
     def check_coc(m):
         return "agree" in m.content.lower() and m.channel == onboard_channel
 
-    coc_pending_log = await spit_log(f'Conduct agreement sent to the welcome channel for {member.mention}. Awaiting acknowledgement...',
-                                     title=f"⏸ Onboarding {member.name}... Step 4 pending...", status=Status.warning)
+    coc_pending_log = await spit_log(
+        f'Conduct agreement sent to the welcome channel for {member.mention}. Awaiting acknowledgement...',
+        title=f"⏸ Onboarding {member.name}... Step 4 pending...",
+        status=Status.warning)
 
     coc_pending = await bot.wait_for('message', check=check_coc)
-    await onboard_channel.send(f"Thank you for your agreement, {member.name}!".format(coc_pending))
+    await onboard_channel.send(
+        f"Thank you for your agreement, {member.name}!".format(coc_pending))
     await coc_pending_log.delete()
 
     await spit_log(f'Conduct acknowledged by {member.mention}!',
-                   title=f"⏬ Onboarding {member.name}... Step 4 complete!", status=Status.success)
+                   title=f"⏬ Onboarding {member.name}... Step 4 complete!",
+                   status=Status.success)
 
     # 5. wait for email address
     await pause(onboard_channel, 2)
     await onboard_channel.send('Now, we need to verify your email address.')
-    email_pending_log = await spit_log(f'Email prompt sent to the welcome channel for {member.mention}. Awaiting email...',
-                                       title=f"⏸ Onboarding {member.name}... Step 5 pending...", status=Status.warning)
+    email_pending_log = await spit_log(
+        f'Email prompt sent to the welcome channel for {member.mention}. Awaiting email...',
+        title=f"⏸ Onboarding {member.name}... Step 5 pending...",
+        status=Status.warning)
 
     async def email_check_bump():
         """
-        Stuck user in this loop until a valid email is provided
-        """
+      Stuck user in this loop until a valid email is provided
+      """
         await pause(onboard_channel, 1)
-        await onboard_channel.send('**Please reply here with your @berkeley.edu email address.**')
+        await onboard_channel.send(
+            '**Please reply here with your @berkeley.edu email address.**')
 
-        email_pending = await bot.wait_for('message', check=lambda m: m.channel == onboard_channel)
+        email_pending = await bot.wait_for(
+            'message', check=lambda m: m.channel == onboard_channel)
         return email_pending.content
 
     email = await email_check_bump()
@@ -159,23 +198,87 @@ async def init_onboard(member):
 
     while not (valid_email(email) and valid_domain(email)):
         """
-        Stuck user in this loop until a valid email is provided
-        """
+      Stuck user in this loop until a valid email is provided
+      """
         if not valid_email(email):
-            await onboard_channel.send(f'**Invalid response** - are you sure this is an email?')
+            await onboard_channel.send(
+                '**Invalid response** - are you sure this is an email?')
         elif not valid_domain(email):
-            await onboard_channel.send(f'**Invalid domain.** Your email must end with {VALID_DOMAIN}.')
+            await onboard_channel.send(
+                f'**Invalid domain.** Your email must end with {VALID_DOMAIN}.'
+            )
 
         email = await email_check_bump()
-
-    await onboard_channel.send(f'Success! We will send an email to {email}. (WIP)')
     await email_pending_log.delete()
 
     await spit_log(f'Email collected from {member.mention}: {email}',
-                   title=f"⏬ Onboarding {member.name}... Step 5 complete!", status=Status.success)
+                   title=f"⏬ Onboarding {member.name}... Step 5 complete!",
+                   status=Status.success)
+
+    # generate random code
+    code = random.randint(100000, 999999)
+    send_code(email, code)
+    await onboard_channel.send(
+        f'Go Bears! :bear: We just sent an email to **{email}**!')
+    code_pending_log = await spit_log(
+        f'Verification code {str(code)} sent to {member.mention} at {email}. Awaiting user code input...',
+        title=f"⏸ Onboarding {member.name}... Step 6 pending...",
+        status=Status.warning)
+
+    async def code_check_bump():
+        """
+      Stuck user in this loop until a valid code is provided
+      """
+        await pause(onboard_channel, 1)
+        await onboard_channel.send(
+            '**Please reply here with the 6-digit code.**')
+
+        email_pending = await bot.wait_for(
+            'message', check=lambda m: m.channel == onboard_channel)
+        return email_pending.content
+
+    user_code = await code_check_bump()
+
+    while not user_code == str(code):
+        await onboard_channel.send(
+            'Incorrect code! Try again? (If you are stuck here, don\'t be afraid to hit the SOS reaction on the first messsage of this channel to request help!)'
+        )
+        user_code = await code_check_bump()
+
+    await code_pending_log.delete()
+
+    await spit_log(f'{member.mention} email {email} verified successfully!',
+                   title=f"⏬ Onboarding {member.name}... Step 6 complete!",
+                   status=Status.success)
 
     # onboarding complete
     WATCH_SOS.remove(welcome_message)
+    await spit_log(f'{member.mention} removed from SOS watchlist.',
+                   title=f"✅ {member.name} Onboarding Complete!",
+                   status=Status.success)
+
+    await onboard_channel.send(
+        '🎉 Verification complete! You are now good to go. Be sure to reach out to staff if you have any questions!'
+    )
+
+    member_role = discord.utils.get(guild.roles, id=MEMBER_ROLE_ID)
+    await member.add_roles(member_role)
+
+
+def send_code(email, code):
+    message = Mail(from_email='btf@orph.app', to_emails=[email])
+
+    data = {'code': str(code)}
+
+    message.dynamic_template_data = data
+    message.template_id = os.getenv('SENDGRID_TEMPLATE')
+
+    try:
+        sg = SendGridAPIClient(os.getenv('SENDGRID_KEY'))
+        sg.send(message)
+        print("Dynamic Messages Sent!")
+    except HTTPError as e:
+        print(e.to_dict)
 
 
 @bot.event
@@ -185,13 +288,15 @@ async def on_ready():
             break
 
     # Debugging Area - Remove For Production
-    await spit_log(f'{bot.user} has connected to Discord & guild {guild.name}!')
+    await spit_log(f'{bot.user} has connected to Discord & guild {guild.name}!'
+                   )
 
 
 @bot.event
 async def on_member_join(member):
     await spit_log(f'Say welcome to {member.name}!',
-                   title="👋 New member joined!", status=Status.info)
+                   title="👋 New member joined!",
+                   status=Status.info)
     await init_onboard(member)
 
 
@@ -200,10 +305,14 @@ async def on_reaction_add(reaction, user):
     message = reaction.message
     if message in WATCH_SOS and reaction.emoji == "🆘" and user != bot.user:
         # SOS received, inviting staff
-        support_role = discord.utils.get(
-            user.guild.roles, id=SUPPORT_ROLE_ID)
-        await message.channel.send(f"Help requested! A staff member will be here to help you soon. {support_role.mention}")
-        await spit_log(f'{user.name} just requested help in {message.channel.mention}!', title=f"🆘 Help requested by {user.name}!", status=Status.error)
+        support_role = discord.utils.get(user.guild.roles, id=SUPPORT_ROLE_ID)
+        await message.channel.send(
+            f"Help requested! A staff member will be here to help you soon. {support_role.mention}"
+        )
+        await spit_log(
+            f'{user.name} just requested help in {message.channel.mention}!',
+            title=f"🆘 Help requested by {user.name}!",
+            status=Status.error)
         WATCH_SOS.remove(message)
         await message.channel.set_permissions(support_role, view_channel=True)
     pass
@@ -216,7 +325,8 @@ async def onboard(ctx, *, member: discord.Member):
     Manually start the onboarding flow of a user
     """
     await spit_log(f'Say welcome to {member.name}!',
-                   title="👋 New member joined!", status=Status.info)
+                   title="👋 New member joined!",
+                   status=Status.info)
     await init_onboard(member)
 
 
@@ -237,23 +347,27 @@ async def reset_server(ctx):
                 await role.delete()
                 removed_roles.append(role.name)
             except discord.Forbidden:
-                await spit_log(f'The role {role.name} cannot be removed!', status=Status.error)
+                await spit_log(f'The role {role.name} cannot be removed!',
+                               status=Status.error)
 
     if len(removed_roles) > 0:
-        await spit_log(f'Roles {", ".join(removed_roles)} have been swept up!', status=Status.success)
+        await spit_log(f'Roles {", ".join(removed_roles)} have been swept up!',
+                       status=Status.success)
     else:
         await spit_log(f'No roles were removed!', status=Status.info)
 
     # Delete onboarding-created channels
     removed_channels = []
-    verification_category = discord.utils.get(
-        ctx.guild.categories, id=VERIFICATION_CATEGORY_ID)
+    verification_category = discord.utils.get(ctx.guild.categories,
+                                              id=VERIFICATION_CATEGORY_ID)
     for channel in verification_category.channels:
         await channel.delete()
         removed_channels.append(channel.name)
 
     if len(removed_channels) > 0:
-        await spit_log(f'Channels {", ".join(removed_channels)} have been swept up!', status=Status.success)
+        await spit_log(
+            f'Channels {", ".join(removed_channels)} have been swept up!',
+            status=Status.success)
     else:
         await spit_log(f'No channels were removed!', status=Status.info)
 
